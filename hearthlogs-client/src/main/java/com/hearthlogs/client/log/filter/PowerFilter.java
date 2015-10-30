9bf8610d93a2cfc71601e2211e91e0787b509aed
emilic <edmilic@gmail.com>
2015-10-30 07:31:33 -0400

    updating subtype to blocktype

--- hearthlogs-client/src/main/java/com/hearthlogs/client/log/filter/PowerFilter.java
+++ hearthlogs-client/src/main/java/com/hearthlogs/client/log/filter/PowerFilter.java
@@ -9,7 +9,7 @@ import org.springframework.util.StringUtils;
  * Examples:
  *   TAG_CHANGE Entity=[name=Acolyte of Pain id=28 zone=HAND zonePos=7 cardId=EX1_007 player=1] tag=ZONE_POSITION value=6
  *
- *   ACTION_START Entity=[name=Ice Lance id=8 zone=HAND zonePos=1 cardId=CS2_031 player=1] SubType=POWER Index=-1 Target=[name=Azure Drake id=40 zone=PLAY zonePos=4 cardId=EX1_284 player=2]
+ *   ACTION_START Entity=[name=Ice Lance id=8 zone=HAND zonePos=1 cardId=CS2_031 player=1] BlockType=POWER Index=-1 Target=[name=Azure Drake id=40 zone=PLAY zonePos=4 cardId=EX1_284 player=2]
  *       TAG_CHANGE Entity=[name=Azure Drake id=40 zone=PLAY zonePos=4 cardId=EX1_284 player=2] tag=FROZEN value=1
  *   ACTION_END
  *
