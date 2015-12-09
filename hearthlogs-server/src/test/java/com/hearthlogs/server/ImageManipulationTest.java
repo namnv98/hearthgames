4964ce57209ba61897ec7ebe9c57f499ea94fcd3
emilic <edmilic@gmail.com>
2015-12-08 20:09:06 -0500

    updated dependencies

---
+++ hearthlogs-server/src/test/java/com/hearthlogs/server/ImageManipulationTest.java
@@ -0,0 +1,120 @@
+package com.hearthlogs.server;
+
+import com.hearthlogs.server.game.parse.domain.CardDetails;
+import com.hearthlogs.server.service.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HearthlogsServerApplication.class)
+public class ImageManipulationTest {
+
+    @Autowired
+    CardService cardService;
+
+    @Test
+    public void shouldMakeMinions() throws IOException {
+
+        Collection<CardDetails> cards = cardService.getCardDetails();
+
+        List<CardDetails> minions = cards.stream().filter(card -> "Minion".equals(card.getType())).collect(Collectors.toList());
+
+        File dir = new File("C:\\images\\minons");
+        File[] files = dir.listFiles();
+
+        List<String> cardFiles = new ArrayList<>();
+        if (files != null) {
+            for (File file: files) {
+                String name = file.getName().replace(".png","");
+                cardFiles.add(name);
+            }
+
+        }
+        for (CardDetails minion: minions) {
+            if (!cardFiles.contains(minion.getId())) {
+                System.out.println(minion.getName()+","+minion.getId());
+            }
+        }
+
+        System.out.println();
+
+    }
+
+    @Test
+    public void shouldMakeHeroPowers() throws IOException {
+
+        Collection<CardDetails> cards = cardService.getCardDetails();
+
+        List<CardDetails> heroPowers = cards.stream().filter(card -> "Hero Power".equals(card.getType())).collect(Collectors.toList());
+
+        File dir = new File("C:\\images\\download");
+        File[] files = dir.listFiles();
+
+        List<String> cardFiles = new ArrayList<>();
+        if (files != null) {
+            for (File file: files) {
+                String name = file.getName().replace(".png","");
+                cardFiles.add(name);
+            }
+
+        }
+        for (CardDetails heroPower: heroPowers) {
+            if (cardFiles.contains(heroPower.getId())) {
+                File file = new File("c:\\images\\download\\"+heroPower.getId()+".png");
+                if (file.exists()) {
+//                    FileUtils.copyFile(file, new File("c:\\images\\heropowers\\"+heroPower.getId()+".png"));
+                    System.out.println(".heropower ."+heroPower.getId() + " { background-image: url(\"http://images.hearthlogs.com/hp/"+heroPower.getId() +".png\"); }");
+                }
+//                else {
+//                    System.out.println(heroPower.getId()+" does not exist");
+//                }
+            }
+        }
+        System.out.println();
+    }
+
+
+    @Test
+    public void shouldMakeWeapons() throws IOException {
+
+        Collection<CardDetails> cards = cardService.getCardDetails();
+
+        List<CardDetails> weapons = cards.stream().filter(card -> "Weapon".equals(card.getType())).collect(Collectors.toList());
+
+        File dir = new File("C:\\images\\download");
+        File[] files = dir.listFiles();
+
+        List<String> cardFiles = new ArrayList<>();
+        if (files != null) {
+            for (File file: files) {
+                String name = file.getName().replace(".png","");
+                cardFiles.add(name);
+            }
+
+        }
+        for (CardDetails weapon: weapons) {
+            if (cardFiles.contains(weapon.getId())) {
+                File file = new File("c:\\images\\download\\"+weapon.getId()+".png");
+                if (file.exists()) {
+//                    FileUtils.copyFile(file, new File("c:\\images\\weapons\\"+weapon.getId()+".png"));
+                    System.out.println(".weapon ."+weapon.getId() + " { background-image: url(\"http://images.hearthlogs.com/w/"+weapon.getId() +".png\"); }");
+                }
+//                else {
+//                    System.out.println(heroPower.getId()+" does not exist");
+//                }
+            }
+        }
+        System.out.println();
+    }
+
+
+}
